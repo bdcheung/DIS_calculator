d4f4f1f56978e3f52cb220adc96fb64369b2e981
Brian Cheung <bdcheung@gmail.com>
2013-04-08 21:47:48 -0400

    Finished the time_value calculations

--- maths.rb
+++ maths.rb
@@ -1,11 +1,27 @@
-class item
-	def name
-		puts "What would you like to buy?"
-		gets.chomp
-	end
+puts "What would you like to buy?"
+name = gets.chomp
+puts "How much does it cost?"
+cost = gets.chomp.to_f
+puts "You want to buy #{name} which costs #{cost}."
 
-	def cost
-		puts "How much does it cost?"
-		gets.chomp
+if cost / @income.to_f >= 1
+	true_cost = cost / @income.to_f
+	puts "The true cost is #{true_cost.to_i} years worth of pay"
+elsif cost / @monthly.to_f >= 1
+	true_cost = cost / @monthly.to_f
+	puts "The true cost is #{true_cost.to_i} months worth of pay"
+elsif cost / @weekly.to_f >= 1
+	true_cost = cost / @weekly.to_f
+	puts "The true cost is #{true_cost.to_i} weeks worth of pay"
+elsif cost / @daily.to_f >= 1
+	true_cost = cost / @daily.to_f
+	puts "The true cost is #{true_cost.to_i} days worth of pay"
+elsif cost / @hourly.to_f >= 1
+	true_cost = cost / @hourly.to_f
+	puts "The true cost is #{true_cost.to_i} hours worth of pay"
+elsif cost / @minutely.to_f >= 1
+	true_cost = cost / @minutely.to_f
+	puts "The true cost of this is #{true_cost.to_i} minutes worth of pay"
+else
+	puts "Why do you even bother asking? You know you can afford it, dummy!"
 end
-
